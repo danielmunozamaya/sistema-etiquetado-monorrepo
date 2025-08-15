989ef75863277852b7fe3a6e4c47ece4f72e6de2
danielmunozamaya <danielmunozamaya2@gmail.com>
2025-08-15 21:06:44 +0200

    [SCRIPTS] - Improve remove-git-folder.ps1

--- remove-git-folder.ps1
+++ remove-git-folder.ps1
@@ -1,11 +1,3 @@
-# Comprobar si se está ejecutando como administrador
-$currentIdentity = [Security.Principal.WindowsIdentity]::GetCurrent()
-$principal = New-Object Security.Principal.WindowsPrincipal($currentIdentity)
-if (-not $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Host "Este script debe ejecutarse como administrador. Por favor, reinícialo con permisos de administrador." -ForegroundColor Red
-    exit 1
-}
-
 # Eliminar carpeta .git de la raíz si existe
 $gitFolder = ".git"
 if (Test-Path $gitFolder) {
