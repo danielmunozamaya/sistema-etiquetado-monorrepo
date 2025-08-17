75b0d66b5428243792126f11c0ada2ec630d80c9
danielmunozamaya <danielmunozamaya2@gmail.com>
2025-08-17 09:29:21 +0200

    [OPTIM] - Comment unused methods in AsociacionProduccion controller

--- backend/src/asociacion-produccion/asociacion-produccion.controller.ts
+++ backend/src/asociacion-produccion/asociacion-produccion.controller.ts
@@ -1,7 +1,7 @@
 import {
   Controller,
   Get,
-  Post,
+  // Post,
   Body,
   Patch,
   Param,
@@ -12,10 +12,10 @@ import {
 } from '@nestjs/common';
 
 import { AsociacionProduccionService } from './asociacion-produccion.service';
-import { CreateAsociacionProduccionDto } from './dto/create-asociacion-produccion.dto';
+// import { CreateAsociacionProduccionDto } from './dto/create-asociacion-produccion.dto';
 import { UpdateAsociacionProduccionDto } from './dto/update-asociacion-produccion.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
-import { ComplexQueryDto } from 'src/common/dto/complex-query.dto';
+// import { ComplexQueryDto } from 'src/common/dto/complex-query.dto';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { UserExistsGuard } from 'src/common/guards/user-exists.guard';
 import { UserJWT } from 'src/common/types/auth.types';
@@ -24,11 +24,11 @@ import { UserJWT } from 'src/common/types/auth.types';
 export class AsociacionProduccionController {
   constructor(private readonly service: AsociacionProduccionService) {}
 
-  @UseGuards(JwtAuthGuard, UserExistsGuard)
-  @Post()
-  create(@Body() dto: CreateAsociacionProduccionDto) {
-    return this.service.createWithTransaction(dto);
-  }
+  // @UseGuards(JwtAuthGuard, UserExistsGuard)
+  // @Post()
+  // create(@Body() dto: CreateAsociacionProduccionDto) {
+  //   return this.service.createWithTransaction(dto);
+  // }
 
   @UseGuards(JwtAuthGuard, UserExistsGuard)
   @Get()
@@ -43,20 +43,20 @@ export class AsociacionProduccionController {
     return this.service.findAllRutasEtiquetas(paginationDto);
   }
 
-  @UseGuards(JwtAuthGuard, UserExistsGuard)
-  @Get(':term')
-  findOne(@Param('term') term: string) {
-    return this.service.findOne(term);
-  }
+  // @UseGuards(JwtAuthGuard, UserExistsGuard)
+  // @Get(':term')
+  // findOne(@Param('term') term: string) {
+  //   return this.service.findOne(term);
+  // }
 
-  @UseGuards(JwtAuthGuard, UserExistsGuard)
-  @Post('v2/query')
-  queryAsociacionesV2(
-    @Body() complexQueryDto: ComplexQueryDto,
-    @Query() paginationDto: PaginationDto
-  ) {
-    return this.service.queryAsociacionesV2(complexQueryDto, paginationDto);
-  }
+  // @UseGuards(JwtAuthGuard, UserExistsGuard)
+  // @Post('v2/query')
+  // queryAsociacionesV2(
+  //   @Body() complexQueryDto: ComplexQueryDto,
+  //   @Query() paginationDto: PaginationDto
+  // ) {
+  //   return this.service.queryAsociacionesV2(complexQueryDto, paginationDto);
+  // }
 
   @UseGuards(JwtAuthGuard, UserExistsGuard)
   @Patch(':id')
